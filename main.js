ee94c7c3f3d4976aecd91fd3ea89472883b55c48
Dina1088 <156786365+Dina1088@users.noreply.github.com>
2024-08-30 07:09:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let bar=document.getElementById("bar")
+let nav=document.getElementById("nav")
+nav.style.maxHeight="0px"
+ bar.addEventListener("click",function(){
+    nav.style.maxHeight="500px"
+ })
+ bar.addEventListener("dblclick",function(){
+    nav.style.maxHeight="0px"
+ })
+
+
+
+    
